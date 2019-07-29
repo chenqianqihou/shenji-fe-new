fd5e52102bf242e2c8ac34de61333862332b627c
xushanshan <shanshan.xu@bespinglobal.cn>
2019-07-29 10:04:07 +0800

    fix

---
+++ src/views/audit/personnel/config.js
@@ -0,0 +1,22 @@
+const detailConfig = {
+  base: [{
+    value: 'name',
+    label: '名称'
+  }, {
+    value: 'cardid',
+    label: '证件号码'
+  }, {
+    value: 'sex',
+    label: '性别'
+  }, {
+    value: 'address',
+    label: '联系地址'
+  }, {
+    value: 'email',
+    label: '邮箱'
+  }, {
+    value: 'phone',
+    label: '手机号码'
+  }]
+
+}
