18479670fcb9b29aafd6a7facda7a76a26c91fe8
xushanshan <shanshan.xu@bespinglobal.cn>
2019-08-30 11:29:22 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,7 +16,7 @@ service.interceptors.request.use(
 
     if (store.getters.token) {
       // let each request carry token
-      // ['X-Token'] is a custom headers key
+      // ['AUTHORIZATION'] is a custom headers key
       // please modify it according to the actual situation
       config.headers['AUTHORIZATION'] = store.getters.token
     }
@@ -53,9 +53,9 @@ service.interceptors.response.use(
     if (res.error.returnCode !== 0 && res.error !== 0) {
       if (res.error.returnCode === 40100) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm('登陆过期，请重新登陆', '提示', {
+          confirmButtonText: '重新登陆',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
