892ff949cef595bf6e89ecc24deb1f7687a144c4
xushanshan <shanshan.xu@bespinglobal.cn>
2019-09-17 14:49:03 +0800

    fix

--- src/views/profile/components/modifyPwd.vue
+++ src/views/profile/components/modifyPwd.vue
@@ -34,7 +34,7 @@
         />
       </el-form-item>
       <el-form-item>
-        <el-button type="success" @click="onChangePassword">确认</el-button>
+        <el-button type="primary" @click="onChangePassword">确认</el-button>
       </el-form-item>
     </el-form>
   </div>
